4135ae0ba0be3ce9b82a687621556421a6a22f61
Quantium <andres@quantium.com.mx>
2017-12-23 15:41:25 -0600

    Pimping some things

--- K-meansClustering.py
+++ K-meansClustering.py
@@ -3,13 +3,13 @@ import pandas as pd
 from matplotlib import pyplot as plt
 from sklearn.cluster import KMeans
 
-
+k = 2
 df = pd.read_csv('volcanic.csv')
 x_test = [[4.671,67],[2.885,61],[1.666,90],[5.623,54],[2.678,80],[1.875,60]]
 colors = ['blue','green','yellow','red']
 
 
-kmeans = KMeans(n_clusters=2)
+kmeans = KMeans(n_clusters=k)
 kmeans = kmeans.fit(df)
 labels = kmeans.labels_
 centroids = kmeans.cluster_centers_
@@ -24,10 +24,12 @@ for x in labels:
     plt.scatter(df.iloc[y,0],df.iloc[y,1],color=colors[x])
     y += 1
 
-for x in range(2):
+for x in range(k):
     lines = plt.plot(centroids[x,0],centroids[x,1],'kx')
+    plt.setp(lines,ms=15.0)
+    plt.setp(lines,mew=2.0)
 
-plt.title('2 clusters')
+plt.title(str(k)+' clusters')
 plt.xlabel('eruptions time')
 plt.ylabel('waiting between eruptions')
 plt.show()
