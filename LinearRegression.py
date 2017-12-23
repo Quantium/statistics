11f119a9d7c284c52bc7f7072799f78b5322657c
Quantium <andres@quantium.com.mx>
2017-12-23 12:27:39 -0600

    Father and son heights prediction

---
+++ LinearRegression.py
@@ -0,0 +1,27 @@
+import numpy as np
+import pandas as pd
+from matplotlib import pyplot as plt
+from sklearn.linear_model import LinearRegression
+
+df = pd.read_csv('fatherandsonheights.csv')
+
+print df.head()
+print ""
+print "Training"
+#Set de entrenamiento
+x_train = df['fatherheight'].values[:,np.newaxis]
+y_train = df['sonheight'].values
+
+lm = LinearRegression()
+
+#Train
+lm.fit(x_train, y_train)
+
+#Testing differents parent heigth to predict sons heigth data
+x_test = [[72.8],[61.1],[67.4],[70.2],[75.6],[60.2],[65.3],[59.2]]
+
+predictions = lm.predict(x_test)
+print "Giving the data set"
+print x_test
+print "The predictions are"
+print predictions
